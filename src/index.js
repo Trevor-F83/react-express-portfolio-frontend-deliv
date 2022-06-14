3dc7436d84624ca3266cb7d0b4ce9f4bcdf10711
trevorfaivre <trevorfaivre@hotmail.com>
2022-06-14 15:21:38 -0500

    wip: built pages and components

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { BrowserRouter as Router } from "react-router-dom";
+
+ReactDOM.render(
+  <Router>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Router>,
+  dosument.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Projects.js
@@ -0,0 +1,5 @@
+function Projeccts(props) {
+    return <h1>Projects</h1>
+}
+
+export default Projects;
